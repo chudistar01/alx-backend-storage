0c84cef58ec5a8c53c784db57abe98a7d2f90222
chudistar01 <christianigwebuike31@gmail.com>
2024-08-22 22:59:34 +0000

    sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
 -- SQL script that ranks country origins of ban
 SELECT origin, COUNT(fans) AS nb_fans 
-FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC
